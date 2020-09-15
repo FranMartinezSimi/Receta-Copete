fc8be0bafc2776858a37feeb2a31721fb8906e7a
francisco <jua.martinez@outlook.es>
2020-09-15 01:21:16 -0500

    mostrando bebidas

--- src/components/ListarRecetas.js
+++ src/components/ListarRecetas.js
@@ -1,7 +1,20 @@
 import React, { useContext } from "react";
+import { RecetasContext } from "../context/RecetasContext";
+import Receta from "./Receta";
 
 const ListarRecetas = () => {
-  return <h1>Listado</h1>;
+  const { recetas } = useContext(RecetasContext);
+
+  return (
+    <div className="row mt-5">
+      {recetas.map((receta) => (
+        <Receta 
+        key={receta.idDrink} 
+        receta={receta} 
+        />
+      ))}
+    </div>
+  );
 };
 
 export default ListarRecetas;
